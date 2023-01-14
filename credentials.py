685f28305f233b47fae8f65f3f43b2feccb999ca
joshuan98 <joshua@intuneauto.com>
2023-01-14 17:29:22 +0800

    Initial commit

---
+++ credentials.py
@@ -0,0 +1,5 @@
+GMAIL_USERNAME = "schinner.inc.and.sons@gmail.com"
+GMAIL_PASSWORD = "schinner.inc.and.sons123"
+
+TELEGRAM_API_KEY = "5625808105:AAHYE4er866oIupdd83dlSbo8aoBcCWZ3a0"
+TELEGRAM_CHAT_ID = "859403288"
